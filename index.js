e0c000fb8239206e0ff9063e57ac6d79c7055b52
Abhi <Bh00fie@gmail.com>
2023-03-12 01:51:40 +0000

    added comments to given initial code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Defining Variables
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -5,11 +6,39 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// Defining output directory
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+// Defining Variable linked to HTML template page
 const render = require("./src/page-template.js");
 
+// as the team will be initially be empty, this can be expressed using an empty array that will be filled by the user
+const team = [];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
+// The team will be filled by letting the user choose each member and the output a HTML file
+const teamMembers = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'teamMember',
+            message: 'Who would you like to add?',
+            choices: ['Manager', 'Engineer', 'Intern', 'That is for today.'],
+        },
+    ]).then((response) => {
+        if (response.teamMember === 'Manager') {
+            getManager();
+        } else if (response.teamMember === 'Engineer') {
+            getEngineer();
+        } else if (response.teamMember === 'Intern') {
+            getIntern();
+        } else {
+            if (!fs.existsSync(OUTPUT_DIR)) {
+                fs.mkdirSync(OUTPUT_DIR)
+            } else {
+                fs.writeFileSync(outputPath, render(team), 'utf-8');
+                console.log('HTML file created');
+            }
+        }
+    });
+}
